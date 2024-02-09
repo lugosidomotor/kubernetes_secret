6bb536810b5b47dc0b28cdc8aa2e4cf6e98956d7
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-02-09 21:05:24 +0100

    Create create_random_secrets.sh

---
+++ create_random_secrets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# A namespace beállítása, ahol a szekrétek létre lesznek hozva
+NAMESPACE=default
+
+# A szekrétek száma
+SECRET_COUNT=10
+
+for i in $(seq 1 $SECRET_COUNT); do
+  # Generál egy random stringet a szekret nevének és tartalmának
+  SECRET_NAME="random-secret-$i"
+  RANDOM_VALUE=$(openssl rand -base64 32)
+
+  # Létrehoz egy szekretet a generált értékekkel
+  kubectl create secret generic $SECRET_NAME --from-literal=password=$RANDOM_VALUE -n $NAMESPACE
+  
+  echo "Secret $SECRET_NAME created with random value."
+done
