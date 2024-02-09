a1af5c12dcb3408af45715ad0c38ead44c42344e
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-02-09 22:06:14 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,7 +7,13 @@ backup_secret() {
 
   echo "Új backup secret létrehozása: $backup_secret_name"
 
-  # Először megpróbáljuk létrehozni a backup Secret-et
+  # Először ellenőrizzük, hogy létezik-e az eredeti secret
+  if ! kubectl get secret "$secret_name" -n "$namespace" &> /dev/null; then
+    echo "Hiba: A megadott secret ('$secret_name') nem létezik a '$namespace' namespace-ben."
+    return 1 # Kilépés hiba kóddal
+  fi
+
+  # Megpróbáljuk létrehozni a backup Secret-et
   if ! kubectl get secret "$backup_secret_name" -n "$namespace" &> /dev/null; then
     kubectl get secret "$secret_name" -n "$namespace" -o yaml |
       sed "s/name: $secret_name/name: $backup_secret_name/" |
